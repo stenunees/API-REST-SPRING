84c96c8a3dafa52c8abbc029a0d8330e7c905914
Stephanie Nunes <100435225+stenunees@users.noreply.github.com>
2023-06-12 23:56:09 -0300

    Add files via upload

---
+++ spring-boot-com-mysql/spring-boot-com-mysql/src/main/java/br/com/stephanienunes/springbootcommysql/SpringBootComMysqlApplication.java
@@ -0,0 +1,15 @@
+package br.com.stephanienunes.springbootcommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = "br.com.stephanienunes.springbootcommysql")
+@EntityScan(basePackages = "br.com.stephanienunes.springbootcommysql.model")
+public class SpringBootComMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComMysqlApplication.class, args);
+	}
+
+}
